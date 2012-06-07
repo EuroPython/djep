c5743a25cf7f4a2e2528f55956f60f0b42b65dda
Markus Zapke-Gründemann <markus@keimlink.de>
2012-06-08 00:09:05 +0200

    add new avatar field with new widget and setting

--- pyconde/accounts/forms.py
+++ pyconde/accounts/forms.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 from django import forms
-from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.contrib.auth import forms as auth_forms
+from django.utils.html import escape, conditional_escape
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext_lazy as _
+from easy_thumbnails.files import get_thumbnailer
 
 from userprofiles.forms import RegistrationForm
 from userprofiles.contrib.profiles.forms import ProfileForm as BaseProfileForm
@@ -14,12 +18,14 @@ from ..forms import Submit
 
 
 class ProfileRegistrationForm(RegistrationForm):
+    avatar = forms.ImageField(widget=forms.FileInput, required=False,
+        help_text=Profile()._meta.get_field_by_name('avatar')[0].help_text)
     short_info = forms.CharField(widget=forms.Textarea, required=False)
 
     def __init__(self, *args, **kwargs):
         super(ProfileRegistrationForm, self).__init__(*args, **kwargs)
         account_fields = Fieldset(_('Account data'), Field('username', autofocus="autofocus"), 'password', 'password_repeat')
-        profile_fields = Fieldset(_('Profile'), 'first_name', 'last_name', 'email', 'short_info')
+        profile_fields = Fieldset(_('Profile'), 'first_name', 'last_name', 'email', 'avatar', 'short_info')
         self.helper = FormHelper()
         self.helper.form_class = 'form-horizontal'
         self.helper.layout = Layout(
@@ -30,6 +36,7 @@ class ProfileRegistrationForm(RegistrationForm):
     def save_profile(self, new_user, *args, **kwargs):
         Profile.objects.create(
             user=new_user,
+            avatar=self.cleaned_data['avatar'],
             short_info=self.cleaned_data['short_info']
         )
 
@@ -71,12 +78,59 @@ class PasswordChangeForm(auth_forms.PasswordChangeForm):
             )
 
 
+class AvatarWidget(forms.ClearableFileInput):
+    """Widget for the avatar model field."""
+    template_with_initial = u'%(initial)s<br />%(clear_template)s<br />%(input_text)s: %(input)s'
+
+    def render(self, name, value, attrs=None):
+        """Renders a thumbnail of the image with controls to clear/change it.
+
+        If no image exists just an image upload widget is rendered.
+
+        If an image has already been uploaded a thumbnail will be shown. It's
+        width and/or height are defined by THUMBNAIL_SIZE. In addition a
+        checkbox to delete the image is added.
+        """
+        substitutions = {
+            'initial_text': self.initial_text,
+            'input_text': self.input_text,
+            'clear_template': '',
+            'clear_checkbox_label': self.clear_checkbox_label,
+        }
+        template = u'%(input)s'
+        substitutions['input'] = super(forms.ClearableFileInput, self).render(name, value, attrs)
+
+        if value and hasattr(value, 'url'):
+            template = self.template_with_initial
+            initial = u'<a href="%(url)s"><img src="%(img_url)s" alt="%(alt)s" /></a>'
+            thumbnail = get_thumbnailer(value).get_thumbnail(
+                {'size': (settings.THUMBNAIL_SIZE, settings.THUMBNAIL_SIZE)})
+            initial_substitutions = {
+                'url': escape(value.url),
+                'img_url': u'%s/%s' % (settings.MEDIA_URL, escape(thumbnail)),
+                'alt': _(u'avatar')
+            }
+            substitutions['initial'] = initial % initial_substitutions
+            if not self.is_required:
+                checkbox_name = self.clear_checkbox_name(name)
+                checkbox_id = self.clear_checkbox_id(checkbox_name)
+                substitutions['clear_checkbox_name'] = conditional_escape(checkbox_name)
+                substitutions['clear_checkbox_id'] = conditional_escape(checkbox_id)
+                substitutions['clear'] = forms.CheckboxInput().render(checkbox_name, False, attrs={'id': checkbox_id})
+                substitutions['clear_template'] = self.template_with_clear % substitutions
+
+        return mark_safe(template % substitutions)
+
+
 class ProfileForm(BaseProfileForm):
+    avatar = forms.ImageField(widget=AvatarWidget, required=False,
+        help_text=Profile()._meta.get_field_by_name('avatar')[0].help_text)
+
     def __init__(self, *args, **kwargs):
         super(ProfileForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
-                Div(Field('first_name', autofocus="autofocus"), 'last_name', 'short_info'),
+                Div(Field('first_name', autofocus="autofocus"), 'last_name', 'avatar', 'short_info'),
                 ButtonHolder(Submit('save', _('Change'), css_class='btn-primary'))
             )
 
