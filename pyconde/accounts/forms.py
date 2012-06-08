3563a4daf93b131e54f40448ee9c291c9a426560
Markus Zapke-Gründemann <markus@keimlink.de>
2012-06-08 14:37:43 +0200

    move AvatarWidget to widgets and add size kwarg

--- pyconde/accounts/forms.py
+++ pyconde/accounts/forms.py
@@ -3,10 +3,7 @@ from django import forms
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.contrib.auth import forms as auth_forms
-from django.utils.html import escape, conditional_escape
-from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
-from easy_thumbnails.files import get_thumbnailer
 
 from userprofiles.forms import RegistrationForm
 from userprofiles.contrib.profiles.forms import ProfileForm as BaseProfileForm
@@ -14,6 +11,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, ButtonHolder, Fieldset, Div, Field, HTML
 
 from .models import Profile
+from .widgets import AvatarWidget
 from ..forms import Submit
 
 
@@ -78,57 +76,16 @@ class PasswordChangeForm(auth_forms.PasswordChangeForm):
             )
 
 
-class AvatarWidget(forms.ClearableFileInput):
-    """Widget for the avatar model field."""
-    template_with_initial = u'%(initial)s<br />%(clear_template)s<br />%(input_text)s: %(input)s'
-
-    def render(self, name, value, attrs=None):
-        """Renders a thumbnail of the image with controls to clear/change it.
-
-        If no image exists just an image upload widget is rendered.
-
-        If an image has already been uploaded a thumbnail will be shown. It's
-        width and/or height are defined by THUMBNAIL_SIZE. In addition a
-        checkbox to delete the image is added.
-        """
-        substitutions = {
-            'initial_text': self.initial_text,
-            'input_text': self.input_text,
-            'clear_template': '',
-            'clear_checkbox_label': self.clear_checkbox_label,
-        }
-        template = u'%(input)s'
-        substitutions['input'] = super(forms.ClearableFileInput, self).render(name, value, attrs)
-
-        if value and hasattr(value, 'url'):
-            template = self.template_with_initial
-            initial = u'<a href="%(url)s"><img src="%(img_url)s" alt="%(alt)s" /></a>'
-            thumbnail = get_thumbnailer(value).get_thumbnail(
-                {'size': (settings.THUMBNAIL_SIZE, settings.THUMBNAIL_SIZE)})
-            initial_substitutions = {
-                'url': escape(value.url),
-                'img_url': u'%s/%s' % (settings.MEDIA_URL, escape(thumbnail)),
-                'alt': _(u'avatar')
-            }
-            substitutions['initial'] = initial % initial_substitutions
-            if not self.is_required:
-                checkbox_name = self.clear_checkbox_name(name)
-                checkbox_id = self.clear_checkbox_id(checkbox_name)
-                substitutions['clear_checkbox_name'] = conditional_escape(checkbox_name)
-                substitutions['clear_checkbox_id'] = conditional_escape(checkbox_id)
-                substitutions['clear'] = forms.CheckboxInput().render(checkbox_name, False, attrs={'id': checkbox_id})
-                substitutions['clear_template'] = self.template_with_clear % substitutions
-
-        return mark_safe(template % substitutions)
-
-
 class ProfileForm(BaseProfileForm):
-    avatar = forms.ImageField(widget=AvatarWidget, required=False,
-        help_text=Profile()._meta.get_field_by_name('avatar')[0].help_text)
+    avatar_size = (settings.THUMBNAIL_SIZE, settings.THUMBNAIL_SIZE)
+    avatar_help_text = Profile()._meta.get_field_by_name('avatar')[0].help_text
+    avatar = forms.ImageField(widget=AvatarWidget(size=avatar_size),
+        required=False, help_text=avatar_help_text)
 
     def __init__(self, *args, **kwargs):
         super(ProfileForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
         self.helper.layout = Layout(
                 Div(Field('first_name', autofocus="autofocus"), 'last_name', 'avatar', 'short_info'),
                 ButtonHolder(Submit('save', _('Change'), css_class='btn-primary'))
