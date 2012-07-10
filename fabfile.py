017aeda0a29422854656e75dec9c1098d5d135cd
Horst Gutmann <zerok@zerokspot.com>
2012-07-10 23:00:56 +0200

    House-cleaning in the fabfile

--- fabfile.py
+++ fabfile.py
@@ -15,16 +15,6 @@ env.pip_files = (
 env.manage_py = join(env.proj_root, env.proj_name, 'manage.py')
 
 
-@task
-def test_env():
-    print env
-
-
-@task
-def test_id():
-    srv_run('id')
-
-
 def srv_run(cmd):
     return sudo(cmd, user=env.srv_user)
 
@@ -54,6 +44,9 @@ def compilemessages():
 
 @task
 def upgrade():
+    """
+    Upgrades the server to the latest codebase.
+    """
     update_proj()
     update_requirements()
     syncdb()
@@ -61,29 +54,30 @@ def upgrade():
     build_static_files()
     compilemessages()
     restart_worker()
-
-
-@task
-def update():
-    update_proj()
-    syncdb()
-    migrate()
-    compilemessages()
-    restart_worker()
+    build_docs()
 
 
 @task
 def syncdb():
+    """
+    Executes python manage.py syncdb on the server.
+    """
     manage_py('syncdb --noinput')
 
 
 @task
 def migrate():
+    """
+    Executes python manage.py migrate on the server.
+    """
     manage_py('migrate')
 
 
 @task
 def update_requirements():
+    """
+    Updates the project's requirements based on the requirements.txt file.
+    """
     pip = join(env.root, 'bin', 'pip')
     for reqfile in env.pip_files:
         srv_run('%s install --use-mirrors -E %s -r %s' % (pip, env.root, reqfile))
@@ -91,6 +85,9 @@ def update_requirements():
 
 @task
 def update_proj():
+    """
+    Fetches changes from the repository.
+    """
     with cd(env.proj_root):
         srv_run('git pull')
         srv_run('git checkout -f %s' % env.branch)
@@ -100,6 +97,9 @@ def update_proj():
 
 @task
 def build_static_files():
+    """
+    Compiles less files into css, runs collectstatic and compress.
+    """
     with path('/srv/pyconde/local/bin', behavior="prepend"):
         with cd(env.proj_root):
             srv_run('lessc -x pyconde/static_media/css/style.{less,css}')
@@ -109,11 +109,17 @@ def build_static_files():
 
 @task
 def restart_worker():
+    """
+    Restarts the gunicorn workers managed by supervisord.
+    """
     return supervisorctl('restart pyconde')
 
 
 @task
 def djshell():
+    """
+    Starts a Django shell on the server.
+    """
     return srv_open_shell('%s %s shell' % (
         join(env.root, 'bin', 'python'),
         join(env.proj_root, env.proj_name, 'manage.py')))
@@ -121,12 +127,16 @@ def djshell():
 
 @task
 def loaddata(fixture):
+    """
+    Loads a given fixture name.
+    """
     manage_py('loaddata {0}'.format(fixture))
 
 
 @task
-def build_statics():
-    with lcd('pyconde'):
-        local('lessc static_media/css/style.{less,css}')
-        local('python manage.py collectstatic --noinput -v1 -i bootstrap -i \'.*.less\'')
-        local('python manage.py compress -v0 --force')
+def build_docs():
+    """
+    Rebuilds the sphinx documentation on the server.
+    """
+    with cd(join(env.proj_root, 'docs')):
+        srv_run("source ../../bin/activate && make html")
