0f6f520e6a73b8137ac6ff1ca696d833d85d55f4
Horst Gutmann <zerok@zerokspot.com>
2012-08-08 22:28:58 +0200

    Adds session page and session listing views

--- pyconde/schedule/views.py
+++ pyconde/schedule/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.template.response import TemplateResponse
 
 from ..proposals import models as proposal_models
+from ..conference import models as conference_models
 
 from . import models
 from . import utils
@@ -38,11 +39,48 @@ def session_by_proposal(request, proposal_pk):
         )
 
 
+def sessions_by_tag(request, tag):
+    """
+    Lists all talks with a given tag on a single page.
+    """
+    sessions = models.Session.objects.select_related('location')\
+        .filter(tags__name=tag).order_by('title')
+    return TemplateResponse(
+        request=request,
+        template='schedule/sessions_by_tag.html',
+        context={
+            'tag': tag,
+            'sessions': sessions
+        }
+    )
+
+
+def sessions_by_location(request, pk):
+    """
+    Lists all talks with a given tag on a single page.
+    """
+    location = get_object_or_404(conference_models.Location, pk=pk)
+    sessions = models.Session.objects.select_related('location')\
+        .filter(location=location).order_by('start')
+    return TemplateResponse(
+        request=request,
+        template='schedule/sessions_by_location.html',
+        context={
+            'location': location,
+            'sessions': sessions
+        }
+    )
+
+
 def view_session(request, session_pk):
     """
     Renders all information available about a session.
     """
+    session = get_object_or_404(models.Session, pk=session_pk)
     return TemplateResponse(
         request=request,
-        template='schedule/session_not_available.html'
+        context={
+            'session': session
+        },
+        template='schedule/session.html'
     )
