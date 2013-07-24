596f9752a7956c259217b0528bed924812d0631f
Andi Albrecht <albrecht.andi@gmail.com>
2013-07-24 07:35:41 +0200

    Add admin filter to filter attendees with children.

--- pyconde/accounts/admin.py
+++ pyconde/accounts/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
 
 from . import models
 
 
-admin.site.register(models.Profile,
-    list_display=['user'])
+class WithChildrenFilter(SimpleListFilter):
+    title = 'Anzahl Kinder'
+    parameter_name = 'children'
+
+    def lookups(self, request, model_admin):
+        return (('y', 'mit Kindern'),
+                ('n', 'ohne Kinder'))
+
+    def queryset(self, request, queryset):
+        if self.value() == 'y':
+            queryset = queryset.filter(num_accompanying_children__gt=0)
+        elif self.value() == 'n':
+            queryset = queryset.filter(num_accompanying_children=0)
+        return queryset
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'num_accompanying_children')
+    list_display_links = ('pk', 'user')
+    list_filter = (WithChildrenFilter,)
+
+
+admin.site.register(models.Profile, ProfileAdmin)
