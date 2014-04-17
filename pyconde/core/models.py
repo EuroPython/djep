18c3ec079b5e805f6d0115df55076707bcef48c6
Markus Holtermann <info@markusholtermann.eu>
2014-04-17 10:57:36 +0200

    Add South post_migrate signal to work with permission changes

---
+++ pyconde/core/models.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+try:
+    # register a signal do update permissions every migration.
+    # This is based on app django_extensions update_permissions command
+    from south.signals import post_migrate
+
+    def update_permissions_after_migration(app,**kwargs):
+        """
+        Update app permission just after every migration.
+        This is based on app django_extensions update_permissions management command.
+        """
+        from django.conf import settings
+        from django.db.models import get_app, get_models
+        from django.contrib.auth.management import create_permissions
+
+        create_permissions(get_app(app), get_models(), 2 if settings.DEBUG else 0)
+
+    post_migrate.connect(update_permissions_after_migration)
+except ImportError:
+    pass
