8557cbda0128b2eb65a8dcd71374fc2c6adad288
Markus Holtermann <info@markusholtermann.eu>
2014-06-03 22:31:17 +0200

    Add database usability / helper views

---
+++ adhoc_sqls/schedule_active_speakers_wo_ticket.sql
@@ -0,0 +1,20 @@
+﻿DROP VIEW IF EXISTS
+	schedule_active_speakers_wo_ticket
+CASCADE;
+
+CREATE VIEW
+	schedule_active_speakers_wo_ticket
+AS
+SELECT DISTINCT
+	s.*
+FROM
+	schedule_active_speakers s
+WHERE
+	s.uid NOT IN (
+		SELECT
+			uid
+		FROM
+			schedule_active_speakers_w_ticket
+	)
+ORDER BY
+	s.uid;
