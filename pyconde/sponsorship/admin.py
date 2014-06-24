2bc230102c1800d811d7004e58f50be770ad6a47
Markus Holtermann <info@markusholtermann.eu>
2014-06-24 22:04:56 +0200

    Refers #179 -- Update sponsorship admins

--- pyconde/sponsorship/admin.py
+++ pyconde/sponsorship/admin.py
@@ -4,20 +4,24 @@ from .models import JobOffer, Sponsor, SponsorLevel
 
 
 class JobOfferAdmin(admin.ModelAdmin):
+    list_display = ('title', 'sponsor', 'active',)
+    list_filter=('active',)
     model = JobOffer
 
 admin.site.register(JobOffer, JobOfferAdmin)
 
 
 class SponsorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'level', 'added', 'active',)
+    list_filter=('level', 'active',)
     model = Sponsor
-    list_display = ("name", "level", "added", "active")
-    list_filter=("level", 'active',)
 
 admin.site.register(Sponsor, SponsorAdmin)
 
 
 class SponsorLevelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'order', 'conference',)
+    list_filter=('conference',)
     model = SponsorLevel
 
 admin.site.register(SponsorLevel, SponsorLevelAdmin)
