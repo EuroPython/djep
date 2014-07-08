a482da1753724e6cdc8a9116243d4b78e06c23f9
Markus Holtermann <info@markusholtermann.eu>
2014-07-08 12:25:52 +0200

    Split mail sending into smaller chunks of users and prevent failing on one user breaking the entire process

--- pyconde/accounts/tasks.py
+++ pyconde/accounts/tasks.py
@@ -21,6 +21,11 @@ from .utils import SEND_MAIL_FILTERS, get_addressed_as
 LOG = logging.getLogger(__name__)
 
 
+def chunks(l, n):
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
+
 @app.task(ignore_result=True)
 def sendmail_task(target, subject, message, domain):
     users = User.objects.select_related('profile').all()
@@ -41,18 +46,23 @@ def sendmail_task(target, subject, message, domain):
     # Re-use the connection to the server, so that a connection is not
     # implicitly created for every mail, as described in the docs:
     # https://docs.djangoproject.com/en/dev/topics/email/#sending-multiple-emails
+    for i, userset in enumerate(chunks(users, 50)):
+        sendmail_task_users.delay(userset, subject, base_message)
+        LOG.info('Delayed sendmail_task_users {0}'.format(i))
+
+
+@app.task(ignore_result=True)
+def sendmail_task_users(users, subject, base_message):
     with closing(mail.get_connection()) as connection:
-        cnt = 0
-        for user in users:
-            addressed_as = get_addressed_as(user)
-            body = base_message.replace('$$RECEIVER$$', addressed_as)
-            to = formataddr((addressed_as, user.email))
-            email = mail.EmailMessage(subject, body, to=[to],
-                headers={'Reply-To': settings.SUPPORT_EMAIL},
-                connection=connection
-            )
-            email.send()
-            cnt += 1
-            if cnt % 50 == 0:
-                LOG.info('Wrote {0} mails'.format(cnt))
-        LOG.info('Wrote {0} mails in total'.format(cnt))
+        for i, user in enumerate(users):
+            try:
+                addressed_as = get_addressed_as(user)
+                body = base_message.replace('$$RECEIVER$$', addressed_as)
+                to = formataddr((addressed_as, user.email))
+                email = mail.EmailMessage(subject, body, to=[to],
+                    headers={'Reply-To': settings.SUPPORT_EMAIL},
+                    connection=connection
+                )
+                email.send()
+            except Exception as e:
+                LOG.error('Error sending mail "{0}" to "{1}": {2}'.format(subject, user, e))
