94e693311fa4f780bd6199ddb2e0adfea8e67d25
Markus Holtermann <info@markusholtermann.eu>
2014-07-15 15:07:42 +0200

    Fixes EP-1044 -- Remove JIRA support form and replace it by mail address
    There is no way to force users to enter a mail address. And even it
    there would be a way, we cannot bind this to the sender mail for easy
    replying via JIRA.

--- pyconde/conference/context_processors.py
+++ pyconde/conference/context_processors.py
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf import settings
+
 from . import models
 
 
 def current_conference(request):
     return {
-        'current_conference': models.current_conference()
+        'current_conference': models.current_conference(),
+        'SUPPORT_EMAIL': settings.SUPPORT_EMAIL,
     }
